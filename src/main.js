077e98ae6355ec4c93cb02844d23d1ccccd22cc1
masa5555 <masa57887@gmail.com>
2020-10-31 16:50:58 +0900

    disjunctionに対応

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var rerejs_1 = require("rerejs");
-var out = new Array;
-var parse2edge = function (pattern) {
+var parse2edge = function (pattern, output, num) {
     switch (pattern.type) {
         case "Pattern":
-            parse2edge(pattern.child);
+            parse2edge(pattern.child, output, num);
             break;
         case "Char":
-            out.push({ from: pattern.range[0], to: pattern.range[1], char: pattern.raw });
+            output.push({ from: num, to: num + 1, char: pattern.raw });
+            break;
+        case "Disjunction":
+            output.push({ from: num, to: num + 1, char: 'ε' });
+            output.push({ from: num, to: num + 2, char: 'ε' });
+            output.push({ from: num + 1, to: num + 3, char: pattern.children[0].raw });
+            output.push({ from: num + 2, to: num + 4, char: pattern.children[1].raw });
+            output.push({ from: num + 3, to: num + 5, char: 'ε' });
+            output.push({ from: num + 4, to: num + 5, char: 'ε' });
             break;
         /*
         case "Seqence":
@@ -20,17 +27,22 @@ var parse2edge = function (pattern) {
 var main = function () {
     var parser1 = new rerejs_1.Parser('a');
     var pattern1 = parser1.parse();
-    //console.log(pattern1);
-    parse2edge(pattern1);
-    console.log(out);
+    var out1 = new Array;
+    parse2edge(pattern1, out1, 0);
+    console.log(out1);
     /*
-    出力：[ { from: 0, to: 1, char: 'a' } ]
+    [ { from: 0, to: 1, char: 'a' } ]
+    */
+    var parser2 = new rerejs_1.Parser('a|b');
+    var pattern2 = parser2.parse();
+    //console.log(pattern2.child);
+    var out2 = new Array;
+    parse2edge(pattern2, out2, 0);
+    console.log(out2);
+    /*
+    
     */
     /*
-    const parser2 = new Parser('(a|b)');
-    const pattern2 = parser2.parse();
-    console.log(pattern2);
-
     const parser3 = new Parser('a+');
     const pattern3 = parser2.parse();
     console.log(pattern3);
