95d58c7ed8a3d3c6196e5e5c6e5e26bb04711812
masa5555 <masa57887@gmail.com>
2020-10-31 17:09:07 +0900

    'a|b', 'a*'に対応させた

--- src/main.ts
+++ src/main.ts
@@ -23,11 +23,9 @@ const parse2edge = (pattern: any, output: Array<edge>, num: number) => {
             output.push({from: num+3, to:num+5, char: 'ε'});
             output.push({from: num+4, to:num+5, char: 'ε'});
             break;
-        /*
-        case "Seqence":
-            console.log("Seqence");
+        case "Many":
+            output.push({from: num, to: num, char: pattern.child.raw});
             break;
-        */
     }
 } 
 
@@ -46,20 +44,31 @@ const main = () => {
 
     const parser2 = new Parser('a|b');
     const pattern2 = parser2.parse();
-    //console.log(pattern2.child);
+    console.log(pattern2.child);
 
     var out2: Array<edge> = new Array;
     parse2edge(pattern2, out2, 0);
-    console.log(out2);
 
     /*
-    
+    [
+        { from: 0, to: 1, char: 'ε' },
+        { from: 0, to: 2, char: 'ε' },
+        { from: 1, to: 3, char: 'a' },
+        { from: 2, to: 4, char: 'b' },
+        { from: 3, to: 5, char: 'ε' },
+        { from: 4, to: 5, char: 'ε' }
+    ]
     */
 
-    /*
-    const parser3 = new Parser('a+');
-    const pattern3 = parser2.parse();
+    const parser3 = new Parser('a*');
+    const pattern3 = parser3.parse();
     console.log(pattern3);  
+
+    var out3: Array<edge> = new Array;
+    parse2edge(pattern3, out3, 0);
+    console.log(out3);
+    /*
+    [ { from: 0, to: 0, char: 'a' } ]
     */
 };
 
